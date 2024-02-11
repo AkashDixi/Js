3c3944aab877167580fe7d966c171afcf44a6627
AKASH DIXIT <124429845+AkashDixi@users.noreply.github.com>
2024-02-11 15:25:14 +0000

    Conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,23 @@
+let score = "35abc";
+let n = null;
+//console.log(typeof (score));
+
+let valueInNumber =Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+let k =Number(n);
+//console.log(typeof k);
+//console.log(k);
+
+// CONVERSION TO NUMBER DATATYPE
+// "33"=> 33 ✔
+//" 33abc"=> NAN type still shows Number
+// True => 1 
+//False => 0
+
+// CONVERSION TO BOOLEAN 
+//let isL="";   // Converted to false             "1"=>True   "0"=> False
+let isL="Akash" // Converted to true
+let isBl=Boolean(isL);
+//console.log(isBl);
+
